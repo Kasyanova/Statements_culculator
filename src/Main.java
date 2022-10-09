2cb4aa31ec5700207a13d98389204bc884098fad
KKasyanova <eliseeva.kseniya@gmail.com>
2022-10-09 15:12:24 +0500

    задача 5 калькулятор

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+package com.max.idea;
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        double one = in.nextDouble();
+
+        System.out.println("Введите второе число: ");
+        double two = in.nextDouble();
+
+        System.out.println("Введите знак (‘+’, ‘-’, ‘*’ или ‘/’): ");
+        char symb = in.next().charAt(0);
+
+        double res = 0;
+        switch (symb) {
+            case '+':
+                res = one+two;
+                break;
+            case '-'  :
+                res = one-two;
+                break;
+            case '*'  :
+                res = one*two;
+                break;
+            case '/'  :
+                res = one/two;
+                break;
+        }
+        System.out.println(res);
+
     }
 }
